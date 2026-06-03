9615a2ff418b03ab0be0de150e699c625748f2d3
Rivangel <rivangel18z@gmail.com>
2026-06-02 19:24:37 -0600

    feat: configure port through environment variable

--- codigo_fuente/app.js
+++ codigo_fuente/app.js
@@ -10,8 +10,7 @@ app.get('/', (req, res) => {
     });
 });
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en puerto ${PORT}`);
 });
